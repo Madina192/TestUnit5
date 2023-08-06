6128cac746b2f318b2882936a4f5bb9d2917185c
Madina <madinaakpaeva000@gmail.com>
2023-08-06 22:45:59 +0600

    added divide fun

--- app/src/main/java/com/example/testunit5/Math.kt
+++ app/src/main/java/com/example/testunit5/Math.kt
@@ -1,9 +1,48 @@
 package com.example.testunit5
 
+import androidx.core.text.isDigitsOnly
+import java.text.NumberFormat
+import java.text.ParsePosition
+
 class Math {
-    fun add(a : String, b : String) : String {
+    fun add(a: String, b: String): String {
         var result = ""
-        result = (a.toInt() + b.toInt()).toString()
+        result = if (a.isEmpty() || b.isEmpty()) {
+            "You didn't enter anything!"
+        } else if (a.contains("..") || b.contains("..")) {
+            "You entered two points!"
+        } else if (a.contains(".") || b.contains(".")) {
+            val doubleResult = (a.toDouble() + b.toDouble())
+            if (doubleResult % 1 > 0.0) {
+                (a.toDouble() + b.toDouble()).toString()
+            } else {
+                (a.toDouble() + b.toDouble()).toInt().toString()
+            }
+        } else if (a.contains(" ") || b.contains(" ")) {
+            (a.replace(" ", "").toInt() + b.replace(" ", "").toInt()).toString()
+        } else if (!a.isNumeric() || !b.isNumeric()) {
+            "You can't enter letters!"
+        } else {
+            (a.toInt() + b.toInt()).toString()
+        }
         return result
+    }
+
+    fun divide(a: String, b: String): String {
+        var result = ""
+        result = if (b == "0") {
+            "You can't divide by 0!"
+        } else if (a.isEmpty() || b.isEmpty()) {
+            "You didn't enter anything!"
+        } else {
+            (a.toInt() / b.toInt()).toString()
+        }
+        return result
+    }
+
+    private fun String.isNumeric(): Boolean {
+        val pos = ParsePosition(0)
+        NumberFormat.getInstance().parse(this, pos)
+        return this.length == pos.index
     }
 }
--- app/src/test/java/com/example/testunit5/MathTest.kt
+++ app/src/test/java/com/example/testunit5/MathTest.kt
@@ -6,7 +6,7 @@ import org.junit.Before
 import org.junit.Test
 
 class MathTest {
-    var math : Math ?= null
+    var math: Math? = null
 
     @Before
     fun init() {
@@ -16,6 +16,53 @@ class MathTest {
     @Test
     fun simpleAdd() {
         assertEquals("5", math?.add("2", "3"))
+    }
+
+    @Test
+    fun doubleAdd() {
+        assertEquals("5", math?.add("2", "3.0"))
+    }
+
+    @Test
+    fun doubleHardAdd() {
+        assertEquals("5.2", math?.add("2.2", "3"))
+    }
+
+    @Test
+    fun doubleDotAdd() {
+        assertEquals("You entered two points!", math?.add("4..1", "0"))
+    }
+
+    @Test
+    fun doubleSymbolAdd() {
+        assertEquals("You can't enter letters!", math?.add("hello15", "9"))
+    }
+
+    @Test
+    fun doubleEmptyAdd() {
+        assertEquals("You didn't enter anything!", math?.add("7", ""))
+    }
+
+    @Test
+    fun doubleSpaceAdd() {
+        assertEquals("50", math?.add("2 ", "4 8"))
+    }
+
+
+    @Test
+    fun simpleDivide() {
+        assertEquals("3", math?.divide("12", "4"))
+    }
+
+
+    @Test
+    fun zeroDivide() {
+        assertEquals("You can't divide by 0!", math?.divide("25", "0"))
+    }
+
+    @Test
+    fun emptyDivide() {
+        assertEquals("You didn't enter anything!", math?.divide("6", ""))
     }
 
     @After
